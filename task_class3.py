c86f184fd4893c501d0035240e2142df67df4df0
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-03-21 16:15:27 +0100

    Update task_class3.py

--- task_class3.py
+++ task_class3.py
@@ -30,3 +30,35 @@ print(weatherData['main']['temp_max'])
 # more???????????
 
 st.text(weatherData['main']['temp_max'])
+
+------------------------------------------------------------
+
+# ! python3
+import json, requests 
+
+# add your own APIkey
+APIkey = '9d5143a6d6866b4cfcde6299838c91aa'
+location = input(' insert location ')
+
+# check API documentation to see what structure of URL is needed to access the data
+# http://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
+# print(url)
+
+
+# Download the JSON data from OpenWeatherMap.org's API.
+response = requests.get(url)  
+# Uncomment to see the raw JSON text:
+# print(response.text)  
+
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+# Uncomment to see the raw JSON text:
+# print(weatherData) 
+# from pprint import pprint 
+# pprint(weatherData) 
+
+print(weatherData['main']['temp_max']) 
+# more???????????
+st.text(weatherData['main']['temp_max'])
