9297e7ad95c664cce6e6b44c17a25b9c67218eb4
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-03-21 16:51:29 +0100

    Update task_class3.py

--- task_class3.py
+++ task_class3.py
@@ -45,3 +45,8 @@ print(weatherData['main']['temp_max'])
 # more???????????
 st.write(weatherData['main']['temp_max'])
 st.write(weatherData)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
