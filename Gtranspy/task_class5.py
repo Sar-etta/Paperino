ecce7cbf07d6d566bf1b7b3bf1d25625f912f304
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-04-04 16:20:32 +0200

    Update task_class5.py

--- Gtranspy/task_class5.py
+++ Gtranspy/task_class5.py
@@ -1,1 +1,14 @@
+import streamlit as st
+
+from googletrans import Translator
+
+translator=Translator()
+
+word = st.text.input(" Gimme a word you want to translate to italian: ")
+
+trans_it= translator.translate (word, dest= "it")
+
+st.write(trans_it)
+
+
 
