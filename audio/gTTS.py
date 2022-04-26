0ebe31346071a7682b3fd9eccc0bf894e395ff63
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-04-26 13:02:25 +0200

    Update gTTS.py

--- audio/gTTS.py
+++ audio/gTTS.py
@@ -14,18 +14,7 @@ trans= translator.translate (originaltext, dest= targetl)
 
 st.write(trans.text)
 
-
-
 firstaudio=gTTS(trans.text, targetl) 
 firstaudio.save('audiof.mp3')
-
-st.audio(
-
-print('your audio: ')
-ipd.display(ipd.Audio('audiof.mp3')) 
-
-
-audio_file = open('myaudio.ogg', 'rb')
-audio_bytes = audio_file.read()
-
-st.audio(audio_bytes, format='audio/ogg')
+my_audio=open('audiof.mp3', 'rb')
+st.audio(my_audio, format='audio/mp3')
