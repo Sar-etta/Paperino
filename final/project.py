2bcaf365d0220742abe871bc102369131799d07a
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-05-11 10:53:55 +0200

    Create project.py

---
+++ final/project.py
@@ -0,0 +1,56 @@
+pip install tweepy
+
+# import tweepy
+import tweepy as tw
+
+# your Twitter API key and API secret
+my_api_key = "4sjNAG1V9tJ4g0o3JklYvgsuu"
+my_api_secret = "X0RQoYDH3AjwvpBwRxcf8RdTvPg6WtDHDcNHglkI8C0SHqd3s5"
+
+# authenticate
+auth = tw.OAuthHandler(my_api_key, my_api_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+search_query = input("insert a word precedeed by #: ")   #-filter:retweets)
+
+# get tweets from the API
+tweets = tw.Cursor(api.search,
+              q=search_query,
+              lang="en",
+              since="2020-09-16").items(50)
+
+# store the API responses in a list
+tweets_copy = []
+for tweet in tweets:
+    tweets_copy.append(tweet)
+    
+print("Total Tweets fetched:", len(tweets_copy))
+
+import pandas as pd
+
+# intialize the dataframe
+tweets_df = pd.DataFrame()
+
+# populate the dataframe
+for tweet in tweets_copy:
+    hashtags = []
+    try:
+        for hashtag in tweet.entities["hashtags"]:
+            hashtags.append(hashtag["text"])
+        text = api.get_status(id=tweet.id, tweet_mode='extended').full_text
+    except:
+        pass
+    tweets_df = tweets_df.append(pd.DataFrame({'user_name': tweet.user.name, 
+                                               'user_location': tweet.user.location,\
+                                               'user_description': tweet.user.description,
+                                               'user_verified': tweet.user.verified,
+                                               'date': tweet.created_at,
+                                               'text': text, 
+                                               'hashtags': [hashtags if hashtags else None],
+                                               'source': tweet.source}))
+    tweets_df = tweets_df.reset_index(drop=True)
+
+# show the dataframe
+tweets_df.head()
+
+print(tweets_df["text"])
