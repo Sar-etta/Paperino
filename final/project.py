4bdd0a3a6404375018ab8d7264de2c5dd32a50dc
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-05-16 16:21:16 +0200

    Update project.py

--- final/project.py
+++ final/project.py
@@ -1,6 +1,7 @@
 import tweepy as tw
 import streamlit as st
 import pandas as pd
+import TextBlob
 
 st.title("try-out project")
 
@@ -60,3 +61,29 @@ for tweet in tweets_copy:
 st.write(tweets_df.head())
 
 st.text(tweets_df["text"])
+
+
+#from textblob import TextBlob
+def sentiment_analysis(tweet):
+ def getSubjectivity(text):
+   return TextBlob(text).sentiment.subjectivity
+  
+ #Create a function to get the polarity
+ def getPolarity(text):
+   return TextBlob(text).sentiment.polarity
+  
+ #Create two new columns ‘Subjectivity’ & ‘Polarity’
+ tweet["TextBlob_Subjectivity"] =    tweet["tweet"].apply(getSubjectivity)
+ tweet ["TextBlob_Polarity"] = tweet["tweet"].apply(getPolarity)
+ def getAnalysis(score):
+  if score < 0:
+    return 'Negative'
+  elif score == 0:
+    return 'Neutral'
+  else:
+    return 'Positive'
+ tweet ["TextBlob_Analysis"] = tweet  ["TextBlob_Polarity"].apply(getAnalysis )
+ return tweet
+
+for row in tweets_df["text"]:
+    sentiment_analysis('tweet')
