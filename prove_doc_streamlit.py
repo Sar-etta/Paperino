df987d063b3313875ee9b1cb7af7e31365c34f79
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-05-23 23:34:12 +0200

    Update prove_doc_streamlit.py

--- prove_doc_streamlit.py
+++ prove_doc_streamlit.py
@@ -4,6 +4,7 @@ with st.container():
     st.write("This is inside the container")
 
     # You can call any Streamlit command, including custom components:
-    st.bar_chart(np.random.randn(50, 3))
+    st.write("surprise")
+    st.balloons
 
 st.write("This is outside the container")
