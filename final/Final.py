edc07dc8ff0d886c1c79c7b3f87a3bc0a7519275
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-05-23 23:29:36 +0200

    Update Final.py

--- final/Final.py
+++ final/Final.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.image(image, caption='Sunrise by the mountains')
 st.title("Sentiment Analysis through Twitter")
 st.caption("The aim of this app is to analyze the sentiment of a certain topic, through an hashtag, and to give as a result a pie-chart that shows summarize the general opinion of it.")
 
