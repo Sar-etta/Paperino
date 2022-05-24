1bc45af4ceb99cc699e34d6008ef29066e436a04
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-05-24 15:14:07 +0200

    Update Final.py

--- final/Final.py
+++ final/Final.py
@@ -7,8 +7,8 @@ import streamlit as st
 #st.image(image, caption='Enter any caption here')
 
 
-#urlfoto = "https://raw.githubusercontent.com/Sar-etta/Paperino/blob/main/final/twitter_logo_banner.jpg"
-#st.image(urlfoto)
+urlfoto = "https://raw.githubusercontent.com/Sar-etta/Paperino/final/twitter_logo_banner.jpg"
+st.image(urlfoto)
 
 st.title("Sentiment Analysis through Twitter")
 st.caption("The aim of this app is to analyze the sentiment of a certain topic, through an hashtag, and to give as a result a pie-chart that shows summarized the general opinion of it.")
