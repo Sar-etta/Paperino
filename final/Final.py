15f078b253671e3385a2742e82ffd6802aa3f3f5
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-05-25 10:20:23 +0200

    Update Final.py

--- final/Final.py
+++ final/Final.py
@@ -136,6 +136,8 @@ translator = Translator()
 
 tweets_df_json = json.dumps(new_tweets_df.text.to_dict()) #new_tweets_df.text.to_json() --> sono la stessa cosa
 st.write(tweets_df_json)
+#TypeError: the JSON object must be str, bytes or bytearray, not NoneType
+#this is the  error, but with the lines before (*type...) on googlecollab it says it is a string, so I don't get what I should do
 
 # Iterate over key/value pairs in dict and print them
 #for key, value in tweets_df_json.items():
