b6868aa9395ab873304a02db808a81e282a36d65
Sar-etta <101562640+Sar-etta@users.noreply.github.com>
2022-06-13 00:59:02 +0200

    Update Final.py

--- final/Final.py
+++ final/Final.py
@@ -132,17 +132,6 @@ from googletrans import Translator
 
 translator = Translator()
 
-#tweets_df_json = json.dumps(new_tweets_df.text.to_dict()) #new_tweets_df.text.to_json() --> sono la stessa cosa
-#st.write(tweets_df_json)
-#TypeError: the JSON object must be str, bytes or bytearray, not NoneType
-#this is the  error, but with the lines before (*type...) on googlecollab it says it is a string, so I don't get what I should do
-
-# Iterate over key/value pairs in dict and print them
-#for key, value in tweets_df_json.items():
-#    st.write(key, ' : ', value)
-# --> con il codice commentato alla linea 137, se provavo a ordinarlo con questo for-loop mi diceva che non si poteva perché non dict
-#MI DA SEMPRE ERROREEEEEEEEE
-
 st.subheader("Here you can translate the Tweets fetched")
 option = st.selectbox('Do you want to translate the tweets?', ('No', 'Yes'))
 if option == 'Yes':
